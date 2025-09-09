e01ade8d43ada1aff8b922085b18625636f71977
Elkin Portocarrero <elkincp5@gmail.com>
2025-09-08 22:18:38 -0500

    refactor(particles): mover componentes de partículas a carpeta components

---
+++ src/ecs/components/particles/BuiltInEmitters.ts
@@ -0,0 +1,15 @@
+import { ParticleComponent } from "atomic-game-engine2d-components";
+
+export class BasicEmitter implements Omit<ParticleComponent, 'type'> {
+    type = 'basic';
+    color = { r: 255, g: 255, b: 255, a: 1 };
+    lifetime = 1.0;
+    emissionRate = 10;
+    speed = 50;
+    spread = Math.PI / 8;
+    size = 4;
+    gravity = { x: 0, y: 0 };
+    layer = 'default';
+    layerMask = 0;
+    visible = true;
+}
